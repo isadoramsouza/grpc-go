3e697bb83c5679a36465d3128564cd058eb89939
Isadora <isadora_msouza@hotmail.com>
2022-02-06 10:10:04 -0300

    Implement bidirectional streaming
    Implement server and client bidirectional streaming

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -20,7 +20,8 @@ func main() {
 	client := pb.NewUserServiceClient(connection)
 	//AddUser(client)
 	//AddUserVerbose(client)
-	AddUsers(client)
+	//AddUsers(client)
+	AddUserStreamBoth(client)
 }
 
 func AddUser(client pb.UserServiceClient) {
@@ -96,4 +97,62 @@ func AddUsers(client pb.UserServiceClient) {
 		log.Fatalf("Could not receive response %v", err)
 	}
 	fmt.Println(res)
+}
+
+func AddUserStreamBoth(client pb.UserServiceClient) {
+	stream, err := client.AddUserStreamBoth(context.Background())
+	if err != nil {
+		log.Fatalf("Could not create request %v", err)
+	}
+
+	reqs := []*pb.User{
+		{
+			Id:    "1",
+			Name:  "Isa",
+			Email: "teste@teste.com",
+		},
+		{
+			Id:    "2",
+			Name:  "Fulano",
+			Email: "teste@teste.com",
+		},
+		{
+			Id:    "3",
+			Name:  "Ciclano",
+			Email: "teste@teste.com",
+		},
+		{
+			Id:    "4",
+			Name:  "Beltrano",
+			Email: "teste@teste.com",
+		},
+	}
+
+	wait := make(chan int)
+
+	go func() {
+		for _, req := range reqs {
+			fmt.Println("Sending User ", req.GetName())
+			stream.Send(req)
+			time.Sleep(time.Second * 3)
+		}
+		stream.CloseSend()
+	}()
+
+	go func() {
+		for {
+			res, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalf("Could not receive stream %v", err)
+				break
+			}
+			fmt.Printf("Receiving user %v with status %v\n", res.GetUser().GetName(), res.GetStatus())
+		}
+		close(wait)
+	}()
+
+	<-wait
 }
--- services/user.go
+++ services/user.go
@@ -83,3 +83,22 @@ func (*UserService) AddUsers(stream pb.UserService_AddUsersServer) error {
 		fmt.Println("Adding ", req.GetName())
 	}
 }
+
+func (*UserService) AddUserStreamBoth(stream pb.UserService_AddUserStreamBothServer) error {
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			log.Fatalf("Could not receive stream %v", err)
+		}
+		err = stream.Send(&pb.UserResultStream{
+			Status: "Added",
+			User:   req,
+		})
+		if err != nil {
+			log.Fatalf("Could not send stream %v", err)
+		}
+	}
+}
